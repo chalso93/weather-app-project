0ec7a6a16046d00f6551ad0b0e2ae7b32c2f0d1f
chalso93 <chalso93@gmail.com>
2021-11-24 11:33:05 +1000

    Added daily forecast API

--- src/app.js
+++ src/app.js
@@ -126,6 +126,8 @@ function showTemp(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 let temperatureElement = document.querySelector("#temp");
 let degree = `°`;
@@ -169,7 +171,9 @@ function getPosition() {
 let gpsLocation = document.querySelector("#current-location-btn");
 gpsLocation.addEventListener("click", getPosition);
 
-function displayForecast() {
+//Forecast
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = "";
   daysShort.forEach(function (day) {
@@ -185,4 +189,10 @@ function displayForecast() {
   });
   forecastElement.innerHTML = forecastHTML;
 }
-displayForecast();
+
+function getForecast(coordinates) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+getPosition();
